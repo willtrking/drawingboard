69dc230800153652bbe9d787b4f0d32de7f7d09f
William King <willtrking@gmail.com>
2014-09-23 12:52:47 -0700

    Woops

--- drawingboard/app.py
+++ drawingboard/app.py
@@ -4,7 +4,7 @@ from . import create_app
 
 def start():
     app = create_app({})
-    app.run(debug=True,port=9090)
+    app.run(debug=True,port=9090,host='0.0.0.0')
 
 if __name__ == '__main__':
     start()
